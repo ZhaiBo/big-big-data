89606ae4646475da1670ed8d610c89222f24cedc
zhaibo <729963917@qq.com>
2018-12-27 14:06:28 +0800

    Kafka Java APIs.

---
+++ flume/src/main/java/ink/zhiabo/kafka/KafkaConsumer.java
@@ -0,0 +1,49 @@
+package ink.zhiabo.kafka;
+
+import kafka.consumer.Consumer;
+import kafka.consumer.ConsumerConfig;
+import kafka.consumer.ConsumerIterator;
+import kafka.consumer.KafkaStream;
+import kafka.javaapi.consumer.ConsumerConnector;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * Kafka消费者
+ */
+public class KafkaConsumer extends Thread {
+
+    private String topic;
+
+    public KafkaConsumer(String topic) {
+        this.topic = topic;
+    }
+
+    private ConsumerConnector createConnector() {
+        Properties properties = new Properties();
+        properties.put("zookeeper.connect", KafkaProperties.ZK);
+        properties.put("group.id", KafkaProperties.GROUP_ID);
+        return Consumer.createJavaConsumerConnector(new ConsumerConfig(properties));
+    }
+
+    @Override
+    public void run() {
+        ConsumerConnector consumer = createConnector();
+        Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
+        topicCountMap.put(topic, 1);
+
+        Map<String, List<KafkaStream<byte[], byte[]>>> messageStreams = consumer.createMessageStreams(topicCountMap);
+
+        //获取我们每次接收到的数据
+        KafkaStream<byte[], byte[]> stream = messageStreams.get(topic).get(0);
+
+        ConsumerIterator<byte[], byte[]> iterator = stream.iterator();
+
+        while (iterator.hasNext()) {
+            System.out.println(new String(iterator.next().message()));
+        }
+    }
+}
---
+++ flume/src/main/java/ink/zhiabo/kafka/KafkaProperties.java
@@ -0,0 +1,16 @@
+package ink.zhiabo.kafka;
+
+/**
+ * Kafka常用配置文件
+ */
+public class KafkaProperties {
+
+    public static final String ZK = "106.12.97.101:2181";
+
+    public static final String TOPIC = "hello_topic";
+
+    public static final String BROKER_LIST = "106.12.97.101:9092";
+
+    public static final String GROUP_ID = "test_group1";
+
+}
